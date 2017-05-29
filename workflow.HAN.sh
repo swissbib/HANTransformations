c6ae847d157411db4b1e32dc772d8cb2aa270bdf
Basil (lokal) <basil@ub-catmandu.ub.unibas.ch>
2017-05-29 15:54:03 +0200

    Anpassungen zur Automatisierung HAN-Upload.

--- workflow.HAN.sh
+++ workflow.HAN.sh
@@ -2,28 +2,46 @@
 
 # Script that controls the main workflow
 
-basedir=$PWD
+source ./han_transformations.conf
 
-DO_DOWNLOAD=1
-DO_CATMANDU=1
-DO_XSLT=1
-DO_FINISH=1
+
+DO_DOWNLOAD=0
+DO_CATMANDU=0
+DO_XSLT=0
+DO_FINISH=0
+DO_UPLOAD=0
+DO_EMAIL=1
+
+DATE=`date +%Y%m%d`
+BASEDIR=$PWD
+LOG=$PWD/log/han_transformations_$DATE.log
+INFOMAIL=$PWD/han_transformations_infomail.txt
 
 if [ "$DO_DOWNLOAD" == "1" ]; then
-   echo "Downloading Aleph-Sequential"
-   $basedir/download.dsv05.sequential.sh
-   mv $basedir/dsv05.seq $basedir/raw.hanseq/
+   echo "Downloading Aleph-Sequential" >> $LOG
+   #$BASEDIR/download.dsv05.sequential.sh
+   #mv $BASEDIR/dsv05.seq $BASEDIR/raw.hanseq/
+   cp /opt/data/dsv05/dsv05.seq $BASEDIR/raw.hanseq/
 fi
 
 if [ "$DO_CATMANDU" == "1" ]; then
-    echo "Transforming Aleph-Sequential into HAN-Marc"
-    perl transform.seq2hanmarc.pl $basedir/raw.hanseq/dsv05.seq $basedir/raw.hanmarc/gruen.xml $basedir/raw.hanmarc/orange.xml
+    echo "Transforming Aleph-Sequential into HAN-Marc" >> $LOG
+    perl transform.seq2hanmarc.pl $BASEDIR/raw.hanseq/dsv05.seq $BASEDIR/raw.hanmarc/gruen.xml $BASEDIR/raw.hanmarc/orange.xml
 fi
 
 if [ "$DO_XSLT" == "1" ]; then
-$basedir/transform.han2sbmarc.sh $basedir HAN 
+    $BASEDIR/transform.han2sbmarc.sh $BASEDIR HAN  >> $LOG
 fi
 
 if [ "$DO_FINISH" == "1" ]; then
-$basedir/transform.into.1.line.sh $basedir
+    $BASEDIR/transform.into.1.line.sh $BASEDIR >> $LOG
+fi
+
+if [ "$DO_UPLOAD" == "1" ]; then
+    echo "Uploading files to swissbib-Server" >> $LOG
+fi
+
+if [ "$DO_EMAIL" == "1" ]; then
+    cat $LOG | mailx -a "From:basil.marti@unibas.ch" -s "Logfile: HAN-Daten fuer swissbib vom $DATE generiert" $MAIL_EDV
+    cat $INFOMAIL | mailx -a "From:basil.marti@unibas.ch" -s "Infomail: HAN-Daten fuer swissbib vom $DATE generiert" $MAIL_EDV
 fi
