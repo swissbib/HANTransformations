8668e32b649df7cb17719cdefdc0ec2adf7da46e
Basil (lokal) <basil@ub-catmandu.ub.unibas.ch>
2017-06-27 10:49:51 +0200

    Error messages are now inlcuded in the log file (except for perl, which produces too much of them)

--- workflow.HAN.sh
+++ workflow.HAN.sh
@@ -2,7 +2,8 @@
 
 # Script that controls the main workflow
 
-source ./han_transformations.conf
+BASEDIR=/opt/scripts/HANTransformations/
+source $BASEDIR/han_transformations.conf
 
 DO_DOWNLOAD=1
 DO_CATMANDU=1
@@ -19,26 +20,26 @@ if [ "$DO_DOWNLOAD" == "1" ]; then
    echo "Downloading Aleph-Sequential" >> $LOG
    #$BASEDIR/download.dsv05.sequential.sh
    #mv $BASEDIR/dsv05.seq $BASEDIR/raw.hanseq/
-   cp /opt/data/dsv05/dsv05.seq $BASEDIR/raw.hanseq/
+   cp /opt/data/dsv05/dsv05.seq $BASEDIR/raw.hanseq/ &>> $LOG 
 fi
 
 if [ "$DO_CATMANDU" == "1" ]; then
     echo "Transforming Aleph-Sequential into HAN-Marc" >> $LOG
-    perl transform.seq2hanmarc.pl $BASEDIR/raw.hanseq/dsv05.seq $BASEDIR/raw.hanmarc/gruen.xml $BASEDIR/raw.hanmarc/orange.xml
+    perl $BASEDIR/transform.seq2hanmarc.pl $BASEDIR/raw.hanseq/dsv05.seq $BASEDIR/raw.hanmarc/gruen.xml $BASEDIR/raw.hanmarc/orange.xml >> $LOG 
 fi
 
 if [ "$DO_XSLT" == "1" ]; then
-    $BASEDIR/transform.han2sbmarc.sh $BASEDIR HAN  >> $LOG
+    $BASEDIR/transform.han2sbmarc.sh $BASEDIR HAN &>> $LOG
 fi
 
 if [ "$DO_FINISH" == "1" ]; then
-    $BASEDIR/transform.into.1.line.sh $BASEDIR >> $LOG
+    $BASEDIR/transform.into.1.line.sh $BASEDIR &>> $LOG
 fi
 
 if [ "$DO_UPLOAD" == "1" ]; then
     echo "Uploading files to swissbib-Server" >> $LOG
-    scp $BASEDIR/out.swissbib-MARC-1line/gruen_marcxml.format.xml swissbib@sb-us9.swissbib.unibas.ch:/swissbib_index/solrDocumentProcessing/FrequentInitialPreProcessing/data/format_archivaldata/
-    scp $BASEDIR/out.swissbib-MARC-1line/orange_marcxml.format.xml swissbib@sb-us9.swissbib.unibas.ch:/swissbib_index/solrDocumentProcessing/FrequentInitialPreProcessing/data/format_archivaldata/
+    scp $BASEDIR/out.swissbib-MARC-1line/gruen_marcxml.format.xml swissbib@sb-us9.swissbib.unibas.ch:/swissbib_index/solrDocumentProcessing/FrequentInitialPreProcessing/data/format_archivaldata/ &>> $LOG
+    scp $BASEDIR/out.swissbib-MARC-1line/orange_marcxml.format.xml swissbib@sb-us9.swissbib.unibas.ch:/swissbib_index/solrDocumentProcessing/FrequentInitialPreProcessing/data/format_archivaldata/ &>> $LOG 
 fi
 
 if [ "$DO_EMAIL" == "1" ]; then
